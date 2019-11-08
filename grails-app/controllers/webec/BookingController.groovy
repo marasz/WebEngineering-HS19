2474591d0aec337f4796c490bb4b7fd7bed9bf12
mauro <maurostehle@gmail.com>
2019-11-08 10:25:23 +0100

    changes

--- grails-app/controllers/webec/BookingController.groovy
+++ grails-app/controllers/webec/BookingController.groovy
@@ -2,5 +2,17 @@ package webec
 
 class BookingController {
 
+    def probiere(){
+        def room = Room.findByName('1.313')
+        def bookings = Booking.findAllByRoom(room)
+
+        def lastNames = bookings.booker.lastName.unique()
+        render text: lastNames
+//
+//        def booker = Person.findByLastName('Holz')
+//        def result = Booking.findAllByBooker(booker)
+//        render text: result.toString()
+    }
+
     static scaffold = Booking
 }
--- grails-app/init/webec/BootStrap.groovy
+++ grails-app/init/webec/BootStrap.groovy
@@ -12,14 +12,14 @@ class BootStrap {
         Room oben  = new Room(name:"1.313", capacity: 60).save(failOnError:true)
         Room unten = new Room(name:"5.2B16", capacity: 6).save(failOnError:true)
 
-        use(DateGroovyMethods) {
+        use(DateGroovyMethods) { //Closure Function
             Date today = new Date().clearTime();
 
             for (int i=0; i<100; i++) {
                 new Booking(booker: dierk, room: oben, day: today + i, slot: Booking.AM).save(failOnError:true)
             }
 
-            new Booking(booker: dierk, room: oben, day: today, slot: Booking.AM).save(failOnError:true)
+            new Booking(booker: dieter, room: oben, day: today, slot: Booking.AM).save(failOnError:true)
 
         }
 
